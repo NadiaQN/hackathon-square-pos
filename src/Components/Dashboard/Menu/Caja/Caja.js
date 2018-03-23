6198f68f29ea49df93cf905cddcde097d572b258
NadiaQN <quezada.nadia@gmail.com>
2018-03-23 01:56:58 -0300

    agregada tabla de gastos y cuadre de caja

--- src/Components/Dashboard/Menu/Caja/Caja.js
+++ src/Components/Dashboard/Menu/Caja/Caja.js
@@ -1,31 +1,61 @@
 import React from 'react';
-import { Col } from 'react-bootstrap';
+import { Table, Grid, Row, Col, responsive } from 'react-bootstrap';
+import './style.css';
 
 const Caja = () => {
     return(
-        <Col md={8}>
-            <Col md={3}>
-                <h4>Ventas Efectivo:</h4>
-                <p>Venta 1: Valor $2000</p>
-                <p>Venta 2: Valor $5000</p>
-                <p>Venta 3: Valor $8990</p>
-                <p>Venta 4: Valor $1000</p>
-                <h5>TOTAL EFECTIVO: $16990</h5>
-            </Col>
-            <Col md={3}>
-                <h4>Ventas Débito:</h4>
-                <p>Venta 1: Valor $15000</p>
-                <p>Venta 2: Valor $7000</p>
-                <h5>TOTAL DÉBITO: $22000</h5>
-            </Col>
-            <Col md={3}>
-                <h4>Venta Cheque:</h4>
-                <p>Venta 1: Valor $50000</p>
-                <h5>TOTAL CHEQUE: $50000</h5>
-            </Col>
-            <Col md={12}>
-                <h4>TOTAL: $88990</h4>
-            </Col>
+        <Col md={7}>
+           <h2>Cuadre de Caja</h2>
+           <h4>Fecha: 24/03</h4>
+           <Table responsive className='table-box'>
+                <thead>
+                    <th>Ventas Efectivo</th>
+                    <th>Ventas Débito</th>
+                    <th>Ventas Cheques</th>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            Código: R-5 Valor: $12.990
+                        </td>
+                        <td>
+                            Código: F-110 Valor: $25.990
+                        </td>
+                        <td>
+                            Códigp: CH-15 Valor: $11.990
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            Código: R-6 Valor: $2.990
+                        </td>
+                        <td>
+                            Cófigo: F-111 Valor: $15.990
+                        </td>
+                        <td>
+                            Código: CH-16 Valor: $20.000
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <h5>TOTAL EFECTIVO: $15.980</h5>
+                        </td>
+                        <td>
+                            <h5>TOTAL DEBITO: $41.980</h5>
+                        </td>
+                        <td>
+                            <h5>TOTAL CHEQUE: $31.990</h5>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td>
+                            <h4>TOTAL FINAL: $89.950</h4>
+                        </td>
+                    </tr>
+                </tbody>
+            </Table>
         </Col>
     )
 }
--- src/Components/Dashboard/Menu/Gastos/Gastos.js
+++ src/Components/Dashboard/Menu/Gastos/Gastos.js
@@ -1,13 +1,34 @@
 import React from 'react';
-import { Col } from 'react-bootstrap';
+import { Table, Grid, Row, Col, responsive } from 'react-bootstrap';
 
 const Gastos = () => {
     return (
-        <Col md={8}>
+        <Col md={4}>
             <h3>Informe de Gastos</h3>
-            <Col md={3}>
-                
-            </Col>
+            <Table responsive>
+                <thead>
+                    <th>Detalle</th>
+                    <th>Valor</th>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Arriendo</td>
+                        <td>$300.000</td>
+                    </tr>
+                    <tr>
+                        <td>Electricidad</td>
+                        <td>$70.00</td>
+                    </tr>
+                    <tr>
+                        <td>Insumos</td>
+                        <td>$250.000</td>
+                    </tr>
+                    <tr>
+                        <td><h4>TOTAL:</h4></td>
+                        <td><h4>$620.000</h4></td>
+                    </tr>
+                </tbody>
+            </Table>
         </Col>
     )
 }
