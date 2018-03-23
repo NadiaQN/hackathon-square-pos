917ba4cf920641bcc3fe2b7e9a38ad3a47e4e457
NadiaQN <quezada.nadia@gmail.com>
2018-03-23 01:22:07 -0300

    commit para hacer pull

--- src/Components/Dashboard/Menu/Gastos/Gastos.js
+++ src/Components/Dashboard/Menu/Gastos/Gastos.js
@@ -5,6 +5,9 @@ const Gastos = () => {
     return (
         <Col md={8}>
             <h3>Informe de Gastos</h3>
+            <Col md={3}>
+                
+            </Col>
         </Col>
     )
 }
